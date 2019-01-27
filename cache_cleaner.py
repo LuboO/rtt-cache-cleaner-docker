a01f88fb278dd6e17011792a3a0bf5457723f898
xobrati1 <lubomir.obratil@gmail.com>
2019-01-27 11:19:42 +0100

    Changed MySQL database connection creation in parent script

--- cache_cleaner.py
+++ cache_cleaner.py
@@ -10,6 +10,7 @@ import os
 import time
 from rtt_pyutils.RttConfigParser import RttConfigParser
 from rtt_pyutils.Utilities import Utilities
+from rtt_pyutils.MySQLDatabaseInfo import MySQLDatabaseInfo
 
 # Don't change these constants without good reason.
 # TODO: Set this to something like 60 seconds after finishing development
@@ -43,25 +44,22 @@ def clean_cache(cnf_file_path):
     main_cnf = RttConfigParser(cnf_file_path)
     cache_data_dir = main_cnf.safe_get("Local-cache", "Data-directory")
     cache_config_dir = main_cnf.safe_get("Local-cache", "Config-directory")
+    
+    with Utilities.create_mysql_database_connection(MySQLDatabaseInfo.get_from_cnf(main_cnf)) as db:
+        with db.cursor() as cursor:
+            for data_file in sorted(os.listdir(cache_data_dir)):
+                exp_id = int(os.path.splitext(os.path.basename(data_file))[0])
+                cursor.execute("SELECT status FROM experiments WHERE id=%s", (exp_id,))
 
-    db_connection = Utilities.create_mysql_database_connection(main_cnf.get_mysql_db_info())
-
-    with db_connection.cursor() as cursor:
-        for data_file in sorted(os.listdir(cache_data_dir)):
-            exp_id = int(os.path.splitext(os.path.basename(data_file))[0])
-            cursor.execute("SELECT status FROM experiments WHERE id=%s", (exp_id,))
-
-            if cursor.rowcount == 1:
-                row = cursor.fetchone()
-                if row[0] == 'finished':
-                    script_logger.info(f"Deleting cached experiment files: {exp_id}")
-                    delete_cache_files(exp_id, cache_data_dir, cache_config_dir)
-                else:
-                    # This is reported only during development
-                    # TODO: Remove this else branch after testing
-                    script_logger.info(f"Experiment is not yet finished: {exp_id}")
-
-    db_connection.close()
+                if cursor.rowcount == 1:
+                    row = cursor.fetchone()
+                    if row[0] == 'finished':
+                        script_logger.info(f"Deleting cached experiment files: {exp_id}")
+                        delete_cache_files(exp_id, cache_data_dir, cache_config_dir)
+                    else:
+                        # This is reported only during development
+                        # TODO: Remove this else branch after testing
+                        script_logger.info(f"Experiment is not yet finished: {exp_id}")
 
 
 if __name__ == "__main__":
@@ -78,3 +76,4 @@ if __name__ == "__main__":
 
     except Exception as ex:
         script_logger.error(f"unrecoverable error: {ex}")
+
